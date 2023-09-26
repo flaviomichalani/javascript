9a3a322a8c08921d4e85f7e4310d6b5c9e8075c1
Flavio Michalani <145224531+flaviomichalani@users.noreply.github.com>
2023-09-26 18:10:17 -0300

    aulas e exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79.5
+console.log(`A velocidade do seu carro é de ${vel} por hora.`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o sinto de segurança.')
